bcf6e192c780aee3329cf8c8806367cc9d4988b2
AlexSussex <a.patriche@sussex.ac.uk>
2018-04-04 15:33:09 +0100

    modified the advanced loop script

--- advancedLoop.sh
+++ advancedLoop.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-
-declare -a myArray=({1..30})
-
-for i in ${myArray}
+echo "Enter a number: "
+read counter
+myArray=()
+for (( number=1; number <= $counter;number++ ))
 do
-if [ $((i%3)) -eq 0 ]
-then
-echo "Fizz!"
-elif [ $((i%5)) -eq 0 ]
-then
-echo "Buzz!"
-else echo $[{1..30}*{1..30}] | xargs -n30 | column -t
-fi
+	myArray+=($number)
+done
+#myArray[$number]=${number}
+#declare -a myArray=({1..${number}})
+
+for i in "${myArray[@]}"
+do
+	if [[ $((i%3)) -eq 0 && $((i%5)) -eq 0 ]]
+	then
+	echo "FizzBuzz!"
+	elif [ $((i%3)) -eq 0 ]
+	then
+	echo "Fizz!"
+	elif [ $((i%5)) -eq 0 ]
+	then
+	echo "Buzz!"
+	else echo ${i}
+	fi
 done
